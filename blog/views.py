743399172dfe370826dde509aaec88e98a9ae9a3
BoberKrzysztof <k.bober@onet.pl>
2024-08-29 11:10:21 +0200

    All Posts & Single Post Pages

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,5 @@
-from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
-all_posts = [
-
-]
-
-
-# Helper function
-def get_date(post):
-    return post['date']
 
 
 # Create your views here.
@@ -21,13 +11,14 @@ def starting_page(request):
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
         "all_posts": all_posts
     })
 
 
 def post_detail(reqest, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(reqest, 'blog/post-detail.html', {
         "post": identified_post
     })
